e2620a3542a560763f2fe8824b4fd74255bdaeda
Tawaraya13 <pramasatria13@gmail.com>
2026-04-30 17:32:27 +0700

    update version number

--- constant/constant.py
+++ constant/constant.py
@@ -3,10 +3,10 @@ import sys, os
 ocrselector = ['pytesseract']
 translator_selector = ['google']
 app_name = "Japan OCR"
-app_ver = "0.5"
+app_ver = "0.5a"
 if getattr(sys, 'frozen', False):
     base_path = sys._MEIPASS
 else:
-    base_path = os.path.abspath(os.path.dirname(__file__))
+    base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 dicdir = os.path.join(base_path, 'unidic', 'dicdir')
 capture_prompt = "<div style=\"display: flex; justify-content: center; align-items: center; height: 100vh;\"><p style=\"margin: 0;\">Click the Capture button or use Alt + Q</p></div>"
